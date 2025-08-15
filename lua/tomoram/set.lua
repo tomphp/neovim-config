eda635b5987af766de1e6a2c2a47b345c253977e
Tom Oram <tom.oram@jeragm.com>
2025-08-15 17:04:31 +0100

    add keybindings

--- lua/tomoram/set.lua
+++ lua/tomoram/set.lua
@@ -12,6 +12,14 @@ vim.opt.wrap = false
 
 vim.opt.incsearch = false
 
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('i', 'jk', '<C-\\><C-n>', { desc = "Exit insert mode" })
+
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open diagnostic / show error' })
+
+vim.keymap.set('n', '<C-s>', vim.cmd.w, { desc = 'Save file' })
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { desc = 'Save file (insert mode)' })
+
+vim.keymap.set('n', '<leader>bn', vim.cmd.bnext, { desc = 'Next buffer' })
+vim.keymap.set('n', '<leader>bp', vim.cmd.bnext, { desc = 'Previous buffer' })
 
 vim.cmd.colorscheme "catppuccin"
