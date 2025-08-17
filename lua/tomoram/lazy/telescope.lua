ecfa8158ac5d5878404838983bf00994c0800c75
Tom Oram <me@tomoram.io>
2025-08-17 10:43:36 +0100

    add find symbol keybinding

--- lua/tomoram/lazy/telescope.lua
+++ lua/tomoram/lazy/telescope.lua
@@ -7,6 +7,7 @@ return {
     vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
     vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
     vim.keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Telescope buffers" })
+    vim.keymap.set("n", "<leader>fs", telescope.lsp_document_symbols, { desc = "Telescope document symbols" })
     vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Telescope help tags" })
   end
 }
