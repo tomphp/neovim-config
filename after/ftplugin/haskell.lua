aab0786b50bacbf34d89493e43368c83db824235
Tom Oram <me@tomoram.io>
2025-08-18 21:34:30 +0100

    map local leader

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -1,17 +1,23 @@
 local ht = require('haskell-tools')
 local bufnr = vim.api.nvim_get_current_buf()
 local opts = { noremap = true, silent = true, buffer = bufnr, }
+
 -- haskell-language-server relies heavily on codeLenses,
 -- so auto-refresh (see advanced configuration) is enabled by default
-vim.keymap.set('n', '<space>cl', vim.lsp.codelens.run, opts)
+vim.keymap.set('n', '<localleader>cl', vim.lsp.codelens.run, opts)
+
 -- Hoogle search for the type signature of the definition under the cursor
-vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
+vim.keymap.set('n', '<localleader>hs', ht.hoogle.hoogle_signature, opts)
+
 -- Evaluate all code snippets
-vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
+vim.keymap.set('n', '<localleader>ea', ht.lsp.buf_eval_all, opts)
+
 -- Toggle a GHCi repl for the current package
-vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
+vim.keymap.set('n', '<localleader>rp', ht.repl.toggle, opts)
+
 -- Toggle a GHCi repl for the current buffer
-vim.keymap.set('n', '<leader>rf', function()
+vim.keymap.set('n', '<localleader>rf', function()
   ht.repl.toggle(vim.api.nvim_buf_get_name(0))
 end, opts)
-vim.keymap.set('n', '<leader>rq', ht.repl.quit, opts)
+
+vim.keymap.set('n', '<localleader>rq', ht.repl.quit, opts)
--- lua/tomoram/init.lua
+++ lua/tomoram/init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 
 require("tomoram.lazy_init")
 require("tomoram.set")
