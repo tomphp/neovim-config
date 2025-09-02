0912edbb699921d3838cfad239eb009684c03e31
Tom Oram <tom.oram@jeragm.com>
2025-09-02 13:48:10 +0100

    set custom keybindings in current buffer only

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -1,6 +1,7 @@
+print("Loading Haskell ftplugin for buffer: " .. vim.api.nvim_get_current_buf())
+
 local ht = require('haskell-tools')
-local bufnr = vim.api.nvim_get_current_buf()
-local opts = { noremap = true, silent = true, buffer = bufnr, }
+local opts = { noremap = true, silent = true, buffer = true }
 
 -- haskell-language-server relies heavily on codeLenses,
 -- so auto-refresh (see advanced configuration) is enabled by default
