cd908798e6f4fe054acc6a2def58cee6c440d1d2
Tom Oram <tom.oram@jeragm.com>
2026-04-28 11:54:47 +0100

    follow open file in tree

--- lua/tomoram/lazy/tree.lua
+++ lua/tomoram/lazy/tree.lua
@@ -2,7 +2,11 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("nvim-tree").setup()
+    require("nvim-tree").setup {
+      update_focused_file = {
+        enable = true,
+      }
+    }
     local treeapi = require("nvim-tree.api")
     vim.keymap.set("n", "<leader>b", treeapi.tree.toggle, { desc = "Toggle Tree Browser" })
   end
